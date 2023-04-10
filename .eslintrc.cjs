8bc19e9bca893f165d6e66a2ff4f2e3122f014e8
SpicyArepa <infantevladimir@gmail.com>
2023-04-10 15:38:42 -0400

    configured eslint and prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,46 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  overrides: [
+    {
+      extends: ['plugin:@typescript-eslint/recommended-requiring-type-checking'],
+      files: ['*.ts', '*.tsx'],
+      parserOptions: {
+        project: path.join(__dirname, 'tsconfig.json'),
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: path.join(__dirname, 'tsconfig.json'),
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    // 'plugin:prettier/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/consistent-type-imports': [
+      'warn',
+      {
+        prefer: 'type-imports',
+        fixStyle: 'inline-type-imports',
+      },
+    ],
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: ['arrow-function', 'function-declaration', 'function-expression'],
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'react/react-in-jsx-scope': 'off',  
+    'react/jsx-props-no-spreading': 'off',
+  },
+};
+
+module.exports = config;
---
+++ prettier.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import("prettier").Config} */
+const config = {
+    semi: true,
+    trailingComma: 'all',
+    singleQuote: true,
+    printWidth: 95,
+    tabWidth: 2,
+    bracketSpacing: true,
+    jsxSingleQuote: true,
+  };
+  
+  module.exports = config;
